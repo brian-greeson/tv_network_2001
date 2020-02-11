8dd78ff41b53d00a8d9c048b45bdca1d9b42ed81
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-11 09:10:48 -0700

    Add test to mach iteraction patter

--- test/character_test.rb
+++ test/character_test.rb
@@ -3,7 +3,29 @@ require "minitest/pride"
 require "./lib/character"
 
 class CharacterTest < Minitest::Test
-  
+  def test_it_exist
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_instance_of Character, kitt
+  end
+
+  def test_it_has_a_name
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT",  kitt.name
+  end
+
+  def test_it_has_a_actor
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_it_has_a_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal 1000000, kitt.salary
+  end
 
 end
 
